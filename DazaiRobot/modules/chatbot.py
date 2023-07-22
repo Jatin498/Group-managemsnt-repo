cd81dc841bada9dda7d713bde7643102427249ec
SauravKale01 <139038240+SauravKale01@users.noreply.github.com>
2023-07-23 00:46:41 +0530

    Update chatbot.py

--- DazaiRobot/modules/chatbot.py
+++ DazaiRobot/modules/chatbot.py
@@ -143,7 +143,7 @@ __help__ = f"""
  »  /ᴄʜᴀᴛʙᴏᴛ *:* sʜᴏᴡs ᴄʜᴀᴛʙᴏᴛ ᴄᴏɴᴛʀᴏʟ ᴘᴀɴᴇʟ
 """
 
-__mod_name__ = "Cʜᴀᴛʙᴏᴛ"
+__mod_name__ = "ᑕᕼᗩTᗷOT"
 
 
 CHATBOTK_HANDLER = CommandHandler("chatbot", dazai, run_async=True)
