54b28276891d2e4c087698e37b21b5bcdcbb6ecc
SauravKale01 <139038240+SauravKale01@users.noreply.github.com>
2023-07-23 02:36:54 +0530

    Update disasters.py

--- DazaiRobot/modules/disasters.py
+++ DazaiRobot/modules/disasters.py
@@ -654,7 +654,7 @@ dispatcher.add_handler(SUPPORTLIST_HANDLER)
 dispatcher.add_handler(SUDOLIST_HANDLER)
 dispatcher.add_handler(DEVLIST_HANDLER)
 
-__mod_name__ = "Dᴇᴠꜱ"
+__mod_name__ = "ᗪᗴᐯՏ"
 __handlers__ = [
     SUDO_HANDLER,
     SUPPORT_HANDLER,
