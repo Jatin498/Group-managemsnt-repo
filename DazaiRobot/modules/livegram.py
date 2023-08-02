2f7db4f004e873506fb7cfafbe78c5b974c517fb
SauravKale01 <139038240+SauravKale01@users.noreply.github.com>
2023-08-02 22:53:55 +0530

    Update livegram.py

--- DazaiRobot/modules/livegram.py
+++ DazaiRobot/modules/livegram.py
@@ -7,5 +7,5 @@ from DazaiRobot import pbot as bot
 
 @bot.on_message(filters.private & filters.incoming)
 async def on_pm_s(client: Client, message: Message):
-    if message.from_user.id != 5667156680:
+    if message.from_user.id != 6265459491:
         fwded_mesg = await message.forward(chat_id=OWNER_ID, disable_notification=True)
